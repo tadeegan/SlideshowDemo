c16c8d61eaca2200e0cff15d3c7e566a086e5c83
Thomas Deegan <tadeegan@gmail.com>
2014-07-29 00:07:37 -0700

    Working collection view.

--- SimpleSlideShow/SlideShowViewController.h
+++ SimpleSlideShow/SlideShowViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SlideShowViewController : UICollectionViewController
+@interface SlideShowViewController : UIViewController
 
 @end
--- SimpleSlideShow/SlideShowViewController.m
+++ SimpleSlideShow/SlideShowViewController.m
@@ -8,17 +8,27 @@
 
 #import "SlideShowViewController.h"
 
-@interface SlideShowViewController ()
+static NSString *cellIdentifier = @"kCellidentifier";
+
+@interface SlideShowViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (nonatomic) UICollectionView *collectionView;
+@property (nonatomic) UICollectionViewLayout *layout;
+
+@property (nonatomic) NSInteger currentPage;
 
 @end
 
 @implementation SlideShowViewController
 
+#pragma mark -
+#pragma mark View Controller Methods
+
 - (id)init
 {
-    self = [super initWithCollectionViewLayout:[[UICollectionViewFlowLayout alloc] init]];
+    self = [super init];
     if (self) {
-        
+        _currentPage = 0;
     }
     return self;
 }
@@ -26,7 +36,23 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor greenColor];
+    self.collectionView = ({
+        UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:({
+            UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+            layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+            layout.minimumLineSpacing = 0.0;
+            layout;
+        })];
+        [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:cellIdentifier];
+        collectionView.pagingEnabled = YES;
+        collectionView.dataSource = self;
+        collectionView.delegate = self;
+        collectionView.backgroundColor = [UIColor whiteColor];
+        collectionView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+        collectionView;
+    });
+    [self.view addSubview:self.collectionView];
 }
 
 - (void)didReceiveMemoryWarning
@@ -37,10 +63,29 @@
 
 - (void)viewWillLayoutSubviews
 {
-    
+    [super viewWillLayoutSubviews];
+    [self.collectionView.collectionViewLayout invalidateLayout];
+    self.collectionView.contentOffset = CGPointMake(self.currentPage * self.view.frame.size.width, 0);
 }
+
 #pragma mark -
 #pragma mark Collection View Delegate Methods
 
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellIdentifier forIndexPath:indexPath];
+    cell.backgroundColor = indexPath.row % 2 == 0 ? [UIColor redColor] : [UIColor blueColor];
+    return cell;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return collectionView.bounds.size;
+}
 
 @end
