f36bbb65b7dcdfd8771db13d555521310b875e42
Thomas Deegan <tadeegan@gmail.com>
2014-07-29 23:57:32 -0700

    basic slides working but animation wonky

--- SimpleSlideShow/SlideShowViewController.m
+++ SimpleSlideShow/SlideShowViewController.m
@@ -10,7 +10,7 @@
 
 static NSString *cellIdentifier = @"kCellidentifier";
 
-@interface SlideShowViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+@interface SlideShowViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UIScrollViewDelegate>
 
 @property (nonatomic) UICollectionView *collectionView;
 @property (nonatomic) UICollectionViewLayout *layout;
@@ -24,48 +24,33 @@ static NSString *cellIdentifier = @"kCellidentifier";
 #pragma mark -
 #pragma mark View Controller Methods
 
-- (id)init
-{
-    self = [super init];
-    if (self) {
-        _currentPage = 0;
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor greenColor];
+    self.currentPage = 0;
     self.collectionView = ({
         UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:({
             UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
             layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
             layout.minimumLineSpacing = 0.0;
+            layout.minimumInteritemSpacing = 0.0;
             layout;
         })];
         [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:cellIdentifier];
         collectionView.pagingEnabled = YES;
         collectionView.dataSource = self;
         collectionView.delegate = self;
-        collectionView.backgroundColor = [UIColor whiteColor];
         collectionView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
         collectionView;
     });
     [self.view addSubview:self.collectionView];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 - (void)viewWillLayoutSubviews
 {
     [super viewWillLayoutSubviews];
     [self.collectionView.collectionViewLayout invalidateLayout];
-    self.collectionView.contentOffset = CGPointMake(self.currentPage * self.view.frame.size.width, 0);
+    self.collectionView.contentOffset = CGPointMake(self.currentPage * self.view.bounds.size.width, 0);
 }
 
 #pragma mark -
@@ -79,13 +64,27 @@ static NSString *cellIdentifier = @"kCellidentifier";
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellIdentifier forIndexPath:indexPath];
-    cell.backgroundColor = indexPath.row % 2 == 0 ? [UIColor redColor] : [UIColor blueColor];
+    cell.backgroundColor = (indexPath.row % 2 == 0) ? [UIColor redColor] : [UIColor greenColor];
+    UIView * square = ({
+        UIView * view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+        view.backgroundColor = [UIColor whiteColor];
+        view.center = CGPointMake(cell.bounds.size.width / 2, cell.bounds.size.height / 2);
+        view.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
+        view;
+    });
+    NSLog(@"%@",cell);
+    [cell addSubview:square];
     return cell;
 }
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     return collectionView.bounds.size;
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    self.currentPage = scrollView.contentOffset.x / scrollView.frame.size.width;
 }
 
 @end
